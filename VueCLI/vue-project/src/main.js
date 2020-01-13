70bd48a8ef4f5c63c4997f51df328f834833885e
Vlad <vladprodan6@gmail.com>
2020-01-13 09:32:04 +0200

    work

--- VueCLI/vue-project/src/main.js
+++ VueCLI/vue-project/src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.component('counterBtn', {
+    template:
+     `<div>
+        <h2>Рахунок: {{counter}}</h2>
+        <button @click='add'>+1</button>
+    </div>`,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add: function() {
+            this.counter++;
+        }
+    }
 })
+
+new Vue({
+    el: '#app',
+})
+
+
